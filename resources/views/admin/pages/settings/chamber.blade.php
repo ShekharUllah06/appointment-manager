7cf422927e11d7aa2e1aa121da9641ab30b53f53
Zaki Hashem Chowdhur <zaki.hashim.chow@gmail.com>
2017-04-04 00:21:36 +0600

    Created this page as the parent page for chamber related pages.

--- resources/views/admin/pages/settings/chamber.blade.php
+++ resources/views/admin/pages/settings/chamber.blade.php
@@ -7,23 +7,14 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">
-                @if (Auth::guest())
-                Admin <small>Chamber Page</small>
-                @else
-                {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}'s <small>Admin Chamber Page</small>
-                @endif
-
-            </h1>
-            <ol class="breadcrumb">
-                <li class="active">
-                    <i class="fa fa-dashboard"></i> Chamber
-                </li>
-            </ol>
-                @include('notifications.status_message')
-                @include('notifications.errors_message')
+            @yield('chamberHead')
         </div>
     </div>
+    
+<div class="row col-lg-12">   
+        @yield('chamberBody')
 </div>
-
+    
+    @yield('jscript')
+    
 @endsection
