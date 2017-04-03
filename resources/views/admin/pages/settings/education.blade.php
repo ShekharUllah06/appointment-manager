7b8e2e3bb442bbacba6930dcf0fcc29ebee5a344
Zaki Hashem Chowdhur <zaki.hashim.chow@gmail.com>
2017-04-04 00:20:17 +0600

    Deleted top position user info portion.

--- resources/views/admin/pages/settings/education.blade.php
+++ resources/views/admin/pages/settings/education.blade.php
@@ -7,14 +7,6 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">
-                @if (Auth::guest())
-                Admin <small>Education Page</small>
-                @else
-                {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}'s <small>Admin Education Page</small>
-                @endif
-
-            </h1>
             <ol class="breadcrumb">
                 <li class="active">
                     <i class="fa fa-dashboard"></i> Education
