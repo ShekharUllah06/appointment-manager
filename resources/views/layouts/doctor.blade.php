7e7d3bbabc014981f4aca663fd67af8c7bb47334
AllICareisLove1982 <zaki.hashim.chow@gmail.com>
2018-01-24 18:44:49 +0600

    Trello Issue "Left menu section" solved.

--- resources/views/layouts/doctor.blade.php
+++ resources/views/layouts/doctor.blade.php
@@ -24,14 +24,19 @@
 
     <!-- Custom Fonts -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
+    
+    <!-- Custom CSS --->
+    <link href="{{ asset('assets/css/zakiStyle.css') }}" rel="stylesheet">
+    
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-	
+    
+
+
     <script>
         window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
@@ -44,12 +49,12 @@
 <body>
     <?php                                   //Check if user is Doctor or die!
     if(Auth::user()->userType !== 1){
-        print("<div style='background-color: white; color: red;'>");
-        exit("Illegal attempt to access unauthorized page!! Please go back to the login page to login.");
-        print("</div>");
+        print("<h1 style='background-color: white; color: red;'>");
+        exit("Illegal attempt to access unauthorized page!! Please go back to the login page to login again.");
+        print("</h1>");
     }
 
-    $url = $_SERVER['REQUEST_URI'];         //Get Current URL for menu selection
+//    $url = $_SERVER['REQUEST_URI'];         //Get Current URL for menu selection
     
     ?>
     <div id="wrapper">
@@ -100,45 +105,43 @@
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
-                    <li>
+                    <li id="dashboardMenuItem">
                         <a href="{{ url('/doctor') }}"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     
                     <!--Schedule Menu Item-->
-                    <?php if(strpos($url, '/doctor/schedule') !== false){ ?>     <!--Menu selected Highlight-->
-                        <li class="active">
-                    <?php }else{ ?>
-                        <li>
-                    <?php } ?>
-                            <a href="{{ url('/doctor/schedule') }}"><i class="fa fa-fw fa-table"></i> Schedule</a>
-                        </li>
+                    <li id="scheduleMenuItem">
+                        <a href="{{ url('/doctor/schedule') }}"><i class="fa fa-fw fa-table"></i> Schedule</a>
+                    </li>
                     
                     <!--Settings Menu Item-->
-                        <li>
+                   <li id = "settingCollapseMenuItemLI" class="collapsed" data-toggle="collapse" data-target="#submenu-collapse" aria-expanded='false'>
 
-                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded='false'><i class="fa fa-fw fa-wrench"></i> Settings  <span class="caret" style="transform: rotate(-90deg);"></span></a>
-                        <ul class="collapse list-unstyled" id="homeSubmenu">
-                            
+                        <a href="#"><i class="fa fa-fw fa-wrench"></i> Settings  <span class="caret" style="transform: rotate(-90deg);"></span></a>
+                        <ul id="submenu-collapse" name="settingCollapseMenuItemUL" class="sub-menu collapse"  aria-expanded='false' style="height: 0px;">
+                                                                 
                             <!--Chamber Sub-Menu Item-->
-
-                                <li class="">
-                                    <a href="{{ url('/doctor/settings/chamber') }}"><i class="glyphicon glyphicon-briefcase"></i> Chamber</a>
-                                </li>
+                            <li class="" id="chamberSubMenuItem">
+                                <a href="{{ url('/doctor/settings/chamber') }}"><i class="glyphicon glyphicon-briefcase"></i> Chamber</a>
+                            </li>
+                            
                             <!--education Sub-Menu Item-->
-
-                                <li class="">
+                            <li class="" id="educationSubMenuItem">
                                 <a href="{{ url('/doctor/settings/education') }}"><i class="glyphicon glyphicon-book"></i> Education</a>
-                                </li>
+                            </li>
                             
-                            <!--personal-info Sub-Menu Item-->
-                            
-                                <li class="">
+                            <!--personal-info Sub-Menu Item-->                            
+                            <li class="" id="personalInfoSubMenuItem">
                                 <a href="{{ url('/doctor/settings/personal-info') }}"><i class="glyphicon glyphicon-user"></i> Personal Info</a>
                             </li>
-                            <li>
+                            
+                            <!--Specialty Sub-Menu Item-->  
+                            <li class="" id="specialtySubMenuItem">
                                 <a href="{{ url('/doctor/settings/specialties') }}"><i class="glyphicon glyphicon-list-alt"></i> Specialties</a>
                             </li>
-                            <li>
+                            
+                            <!--work-history Sub-Menu Item-->  
+                            <li class="" id="workHistoryPersonalSubMenuItem">
                                 <a href="{{ url('/doctor/settings/work-history') }}"><i class="glyphicon glyphicon-list-alt"></i> Work History</a>
                             </li>
                         </ul>
@@ -161,10 +164,15 @@
         <!-- in page scripts -->
         @yield('jscript')
 
+        
+
     <!-- jQuery -->
     <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
     <!--<script src="https://ajax.googleapis(.)com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
 
+    <!-- Plugin JavaScript -->
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>-->
+    
     <!-- Bootstrap Core JavaScript -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     
@@ -172,6 +180,38 @@
     <script src="{!! asset('assets/js/plugins/morris/raphael.min.js') !!}"></script>
     <script src="{!! asset('assets/js/plugins/morris/morris.min.js') !!}"></script>
     <script src="{!! asset('assets/js/plugins/morris/morris-data.js') !!}"></script>
+    
+    <script>
+
+//        sub-menu selection active
+            if(window.location.href.indexOf("/doctor/schedule") != -1){
+                $('#scheduleMenuItem').addClass('activeMenuItem');
+            }else if(window.location.href.indexOf("/doctor/settings/chamber") != -1){
+                $('#chamberSubMenuItem').addClass('activeMenuItem');
+            }else if(window.location.href.indexOf("/doctor/settings/education") != -1){
+                $('#educationSubMenuItem').addClass('activeMenuItem');
+            }else if(window.location.href.indexOf("/doctor/settings/personal-info") != -1){
+                $('#personalInfoSubMenuItem').addClass('activeMenuItem');
+            }else if(window.location.href.indexOf("/doctor/settings/specialties") != -1){
+                $('#specialtySubMenuItem').addClass('activeMenuItem');
+            }else if(window.location.href.indexOf("/doctor/settings/work-history") != -1){
+                $('#workHistoryPersonalSubMenuItem').addClass('activeMenuItem');
+            }else{
+                $('#dashboardMenuItem').addClass('activeMenuItem');
+            }
+        
+//        Sub-menu hold collapse
+            if(window.location.href.indexOf("/doctor/settings/") != -1){               
+                document.getElementById("settingCollapseMenuItemLI").classList.remove("collapsed");
+                document.getElementById("settingCollapseMenuItemLI").classList.add("active");
+                document.getElementById("settingCollapseMenuItemLI").setAttribute('aria-expanded', 'true');
+                
+                document.getElementById("submenu-collapse").classList.remove("sub-menu");
+                document.getElementById("submenu-collapse").classList.add("in");
+                document.getElementById("submenu-collapse").setAttribute("aria-expanded", "true");
+                document.getElementById("submenu-collapse").setAttribute("style", "");              
+            }
+    </script>
 </body>
 
 </html>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -23,6 +23,7 @@
     
     <!-- Custom CSS --->
     <link href="{{ asset('assets/css/front.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/zakiStyle.css') }}" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
